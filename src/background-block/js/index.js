80950af524443297ec1d3eaa06294784a7404c6a
yanjiaxuan <330544968@qq.com>
2021-04-04 09:18:06 +0800

    * 首页调整

--- src/background-block/js/index.js
+++ src/background-block/js/index.js
@@ -1,5 +1,7 @@
 var isMobile = window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
 
-for (i = 0; i < 400; i++) {
-    document.querySelector('.block-wrapper').innerHTML += `<div class="block ${isMobile ? 'block-mobile' : 'block-pc'}" style="--i: ${i}"></div>`;
-}
+setTimeout(() => {
+    for (i = 0; i < 400; i++) {
+        document.querySelector('.block-wrapper').innerHTML += `<div class="block ${isMobile ? 'block-mobile' : 'block-pc'}" style="--i: ${i}"></div>`;
+    }
+}, 500);
